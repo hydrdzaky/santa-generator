98d56d4bf846158a4891a69e996276ef86a8f931
unknown <haydar.dzaky@gmail.com>
2024-05-13 05:40:06 +0700

    edit main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,21 @@ resource "google_cloud_run_v2_service" "default" {
     }
   }
 }
+
+ data "google_iam_policy" "noauth" {
+   binding {
+     role = "roles/run.invoker"
+     members = ["allUsers"]
+   }
+ }
+
+ resource "google_cloud_run_service_iam_policy" "noauth" {
+   location    = google_cloud_run_v2_service.default.location
+   project     = google_cloud_run_v2_service.default.project
+   service     = google_cloud_run_v2_service.default.name
+
+   policy_data = data.google_iam_policy.noauth.policy_data
+ }
 variable "tags" {
   type=string   
 }  
