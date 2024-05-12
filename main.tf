02353f7dff5e4d78ebcd2e242044318d7c563def
unknown <haydar.dzaky@gmail.com>
2024-05-13 04:50:46 +0700

    edit main.tf

--- main.tf
+++ main.tf
@@ -1,50 +1,31 @@
-resource "google_cloud_run_service" "run_service" {
-  name = "your_app"
-  location = "us-central1"
-
-  template {
-	spec {
-  	containers {
-    	image = "gcr.io/proyekdicoding-416705/secretsanta:${var.tags}"
-  	}
-	}
-  }
-
+provider "google" {
+    project = "proyekdicoding-416705"
+}
 resource "google_project_service" "run_api" {
   service = "run.googleapis.com"
-
   disable_on_destroy = true
 }
-   depends_on = [google_project_service.run_api]
+resource "google_cloud_run_v2_service" "default" {
+  name     = "cloudrun-service"
+  location = "us-central1"
+  ingress = "INGRESS_TRAFFIC_ALL"
+ 
+  template {
+    containers {
+        image = "gcr.io/proyekdicoding-416705/secretsanta:${var.tags}"
+    ports {
+        container_port = 8080
+      }
+    }
+  }
+  depends_on = [google_project_service.run_api]
+}
+resource "google_cloud_run_service_iam_member" "run_all_users" {
+  service  = google_cloud_run_service.run_service.name
+  location = google_cloud_run_service.run_service.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
 variable "tags" {
   type=string   
 } 
-
-
-
-# provider "google" {
-#     project = "proyekdicoding-416705"
-#     region = "us-central1"
-# }
-# resource "google_container_registry" "registry" {
-#   project  = "proyekdicoding-416705"
-#   location = "US"
-# }
-# resource "google_cloud_run_v2_service" "default" {
-#   name     = "cloudrun-service"
-#   location = "us-central1"
-#   ingress = "INGRESS_TRAFFIC_ALL"
- 
-#   template {
-#     containers {
-#         image = "gcr.io/proyekdicoding-416705/secretsanta:${var.tags}"
-#     ports {
-#         container_port = 8080
-#       }
-#     }
-#   }
-# }
-# variable "tags" {
-#   type=string   
-# } 
